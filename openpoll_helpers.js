a027ac2900724f738306741c0a5495db4493f38a
Zack <zack13254@gmail.com>
2018-02-28 19:59:49 -0500

    mainchain helper base

---
+++ openpoll_helpers.js
@@ -0,0 +1,9 @@
+
+// Create our helpers
+var helpers = {};
+
+// Get all of our libraries..
+helpers.mainchain = require( './helpers/mainchain' );
+
+// Export our helpers
+module.exports = helpers;
