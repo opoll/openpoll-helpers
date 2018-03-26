99e784d8122000b6a124af8fd9b8172232cac1df
Ruben Rutten <ruben@rubenrutten.nl>
2018-03-26 16:37:46 +0200

    Added type data for poll methods

--- index.d.ts
+++ index.d.ts
@@ -2,16 +2,25 @@ interface NestedSchemas {
 	[key: string]: NestedSchemas;
 }
 
+interface PollHelpers {
+	validateSchema: (data: any) => boolean;
+	isExpired: (data: any) => boolean;
+	hash: (data: any, digestType?: string) => string;
+}
+
 interface PollResponseHelpers {
 	validateSchema: (data: any) => boolean;
+	validateAnswers: (pollData: any, responseData: any) => boolean;
+	hash: (data: any, digestType?: string) => string;
 	validateSignature: (data: any, publicKey?: string) => boolean;
+	sign: (data: any, privateKey: string | Buffer, publicKey: string | Buffer, rewardAddress?: string) => void;
 }
 
 interface OpenPollHelpers {
 	generic: any;
 	mainChain: any;
 	pollResponse: PollResponseHelpers;
-	poll: any;
+	poll: PollHelpers;
 	shardBlock: any;
 }
 
